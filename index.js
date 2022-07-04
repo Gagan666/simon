3fc97f5ffcfdf894230a42d823bf8d584c4b9ede
Gagan <gaganyadav2002@gmail.com>
2022-07-05 00:04:34 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,90 @@
+var scores = new Array();
+var userclicked = new Array();
+
+var kflag = 1;
+var gflag = 0;
+var ctr=0;
+
+function pickrandomsound() {
+  ctr=0;
+  $("h1").text("Scores:" + scores.length);
+  var arr = ['green', 'red', 'blue', 'yellow'];
+  var elerandom = arr[parseInt(Math.random() * 4)];
+  scores.push(elerandom);
+  soundgen(elerandom);
+}
+
+
+function soundgen(chosenele) {
+  addanimation(chosenele);
+  var aud = new Audio("./sounds/" + chosenele + ".mp3");
+  aud.play();
+}
+
+function validate() {
+if(userclicked[userclicked.length-1]==scores[ctr])
+{
+  ctr++;
+}
+else {
+  gflag=0;
+
+}
+
+}
+
+$(".btn").click(function() {
+  if (gflag) {
+    addanimation(this.id);
+    userclicked.push(this.id);
+    var aud = new Audio("./sounds/" + this.id + ".mp3");
+    aud.play();
+    validate();
+    if (gflag && ctr==scores.length)
+    {
+      setTimeout(function() {
+      pickrandomsound();
+    }, 1000);
+  }
+  }
+  if(gflag!=1)
+ {
+   changebgwrong();
+   $("h1").text("Game Over! Press any key to restart");
+    var aud = new Audio("./sounds/" + "wrong" + ".mp3");
+    aud.play();
+    kflag=1;
+
+  }
+})
+
+
+function changebgwrong()
+{
+  $("body").css("background-color","red");
+  setTimeout(function() {
+    $("body").css("background-color","#011F3F");
+  }, 100);
+}
+
+
+function addanimation(id) {
+  $("#" + id).addClass("pressed");
+  setTimeout(function() {
+    $("#" + id).removeClass("pressed");
+  }, 100);
+}
+
+
+$(document).keypress(function(e) {
+  if (kflag) {
+    kflag = 0;
+    gflag = 1;
+    ctr=0;
+    scores.length=0;
+    userclicked.length=0;
+    pickrandomsound();
+
+  }
+
+})
